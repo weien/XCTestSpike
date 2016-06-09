359a7a3d03309f73378d65828bc66ded1bbb71d8
Weien Wang <weienw@gmail.com>
2016-06-09 11:13:41 -0500

    [NEW] WWWebService

---
+++ XCTestSpike/XCTestSpike/WWWebService.h
@@ -0,0 +1,13 @@
+//
+//  WWWebService.h
+//  XCTestSpike
+//
+//  Created by Weien Wang on 6/9/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WWWebService : NSObject
+
+@end
---
+++ XCTestSpike/XCTestSpike/WWWebService.m
@@ -0,0 +1,13 @@
+//
+//  WWWebService.m
+//  XCTestSpike
+//
+//  Created by Weien Wang on 6/9/16.
+//  Copyright © 2016 Weien Wang. All rights reserved.
+//
+
+#import "WWWebService.h"
+
+@implementation WWWebService
+
+@end
